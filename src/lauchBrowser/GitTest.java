366a86ed33afbf9ba0015b04b361e52cb61ec8ac
tek1125 <ertetch@gmail.com>
2021-11-21 05:43:47 -0600

    3rd commit

--- src/lauchBrowser/GitTest.java
+++ src/lauchBrowser/GitTest.java
@@ -2,7 +2,7 @@ package lauchBrowser;
 
 public class GitTest {
 	
-	public static void main {
+	public static void main (String args) {
 		
 		System.out.println("git test");
 	}
---
+++ src/lauchBrowser/GitTest1.java
@@ -0,0 +1,14 @@
+package lauchBrowser;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+
+	}
+
+}
